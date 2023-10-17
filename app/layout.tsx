c71040ac6e130bc39715ed436f06f16d64108113
Emiliano-Bucci <emiliano.bucci@outlook.com>
2023-10-17 18:39:03 +0200

    feat: add fetch

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,24 @@
-export default function RootLayout({ children }) {
+async function getSiteOptions<T>(): Promise<T> {
+  const res = await fetch("https://catfact.ninja/fact", {
+    next: {
+      revalidate: 2,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("getSiteOptions");
+  }
+
+  return res.json();
+}
+
+export default async function RootLayout({ children }) {
+  const data = await getSiteOptions();
+  console.log({ data });
   return (
     <html>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
